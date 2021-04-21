71beb06afc8c57a021537f179f5cf9652a2813ad
kanghosun <khsuniv201@gmail.com>
2021-04-21 20:33:56 +0900

    leg.py

--- leg.py
+++ leg.py
@@ -6,7 +6,6 @@ from ar_markers import detect_markers
 pipeline = rs.pipeline()
 config = rs.config()
 
-# Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
@@ -19,47 +18,43 @@ if device_product_line == 'L500':
 else:
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
 profile = pipeline.start(config)
 
-# Getting the depth sensor's depth scale (see rs-align example for explanation)
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
 print("Depth Scale is: ", depth_scale)
 
-# We will be removing the background of objects more than
-#  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 1  # 1 meter
+clipping_distance_in_meters = 1
 clipping_distance = clipping_distance_in_meters / depth_scale
 
-# Create an align object
-# rs.align allows us to perform alignment of depth frames to others frames
-# The "align_to" is the stream type to which we plan to align depth frames.
 align_to = rs.stream.color
 align = rs.align(align_to)
 
-# Streaming loop
 try:
     while True:
-        # Get frameset of color and depth
         frames = pipeline.wait_for_frames()
-        # frames.get_depth_frame() is a 640x360 depth image
-
-        # Align the depth frame to color frame
         aligned_frames = align.process(frames)
 
-        # Get aligned frames
-        # aligned_depth_frame is a 640x480 depth image
         aligned_depth_frame = aligned_frames.get_depth_frame()
         color_frame = aligned_frames.get_color_frame()
 
-        # Validate that both frames are valid
         if not aligned_depth_frame or not color_frame:
             continue
 
         depth_image = np.asanyarray(aligned_depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
         
+        height, width, channel = color_image.shape
+        center = (width//2, height//2)
+        c_depth = aligned_depth_frame.get_distance(list(center)[0], list(center)[1])
+        
+        cv2.circle(color_image, center, 5, (255,0,0), -1)
+        cv2.putText(color_image, str(int(c_depth*1000)), 
+                                                center, 0, 
+                                                fontScale=0.8,
+                                                color=(255,0,0),
+                                                thickness=2,
+                                                lineType=cv2.LINE_AA)
         cv2.imshow('test frame', color_image)
         
         key = cv2.waitKey(1)
